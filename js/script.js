85afdf1911b41000115f1a95a1d478ffb0689348
arimototoku <sonne@i.ua>
2021-01-08 12:31:30 +0200

    Add mobile version

--- js/script.js
+++ js/script.js
@@ -3,22 +3,41 @@ const errorValue = document.getElementById('error');
 const email = document.getElementById('email');
 const submit = document.getElementById('submit');
 
+const setErrorMessage = message => {
+    errorValue.style.visibility = "visible";
+    email.style.borderColor = "#FF0000";
+    errorValue.textContent = message;       
+}  
+
+const checkClientWidth = () => {
+    if (window.innerWidth < 400) {
+        submit.style.marginTop = '40px';
+        errorValue.style.position = 'relative';
+        errorValue.style.bottom = '90px';
+    }
+}
+
+const resetErrorValue = () => {
+    email.style.borderColor = '';
+    errorValue.style.visibility = '';
+    submit.style.marginTop = '40px';
+    errorValue.style.position = '';
+    errorValue.style.bottom = '';
+}
+
 const checkEmailField = () => {
-    email.style.borderColor = "";
-    errorValue.style.visibility = "hidden";
+    resetErrorValue();
+    checkClientWidth();
     if (!email.value) {
-        errorValue.style.visibility = "visible";
-        email.style.borderColor = "#FF0000";
-        errorValue.textContent = "Whoops! It looks like you forgot to add your email";
+        setErrorMessage("Whoops! It looks like you forgot to add your email");
         return;
     }
     if (email.value.length < 5 || !email.value.includes('@')) {
-        errorValue.style.visibility = "visible";
-        email.style.borderColor = "#FF0000";
-        errorValue.textContent = "Please provide a valid email address";
+        setErrorMessage("Please provide a valid email address");
     }
 
 }
 
 submit.addEventListener('click', checkEmailField);
 
+console.log(window.innerWidth);
